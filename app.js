4be9a2750e7ca175cdc85bac5b35711b5997d389
m2wasabi <m2wasabi@gmail.com>
2016-11-07 14:13:38 +0900

    listener add

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ firebase.initializeApp({
   serviceAccount: process.env.FIREBASE_SERVICE_ACCOUNT,
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
+
+var db = firebase.database();
+var ref = db.ref("/");
+
+// ref.once("value", function(snapshot) {
+//   console.log(JSON.stringify(snapshot.val()) );
+// });
+
+ref.on("child_changed" , function(snapshot){
+    console.log(snapshot.key);
+    console.log(snapshot.child);
+    console.log(JSON.stringify(snapshot.val()) );
+});
